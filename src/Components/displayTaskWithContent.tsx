a4fa8625bfc43bce706557dc60d3eb7d1af9f305
Felipe <felipecss88@gmail.com>
2022-12-28 09:26:30 -0300

    projeto atualizado

--- src/Components/displayTaskWithContent.tsx
+++ src/Components/displayTaskWithContent.tsx
@@ -5,6 +5,17 @@ export function DisplayTaskWithContent(){
 
     return(
         <div className={styles.displayTaskContainer}>
+            <header>
+                <div>
+                <span className={styles.createdTaskText}>Tarefas criadas</span>
+                <span className={styles.createdTaskCount}> 5 </span>
+                </div>
+                
+                <div >
+                <span className={styles.doneTasksText}>Concluídas</span>
+                <span className={styles.doneTasksStatus}>  2 de 5</span>
+                </div>
+            </header>
 
         </div>
     )
